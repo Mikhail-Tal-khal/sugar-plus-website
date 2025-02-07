718c8d4730bfab5626e5404de2994e9c302010c3
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2025-02-08 02:40:14 +0300

    sugar-plus updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Navbar } from './components/navbar';
 import { Providers } from '@/app/providers';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -27,6 +28,7 @@ export default function RootLayout({
           <Navbar />
           {children}
         </Providers>
+        <SpeedInsights />
       </body>
     </html>
   );
